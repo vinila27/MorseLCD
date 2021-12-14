56b6f75a41302a958c8d9496378cff9118e5e86e
Daniel <danielpsaila974@gmail.com>
2021-12-15 09:32:29 +1300

    Seperated program into 2 operation modes. terminalProg() for writing to LCD via putty terminal. morseProg() for writing to LCD using the button (using internal pull-up resistor)

---
+++ inc/main.h
@@ -0,0 +1,33 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#include "button.h"
+
+static inline void terminalProg(){
+	static uint8_t rxData;
+	static uint8_t address;
+		rxData = receiveUART();
+		enterLetter(rxData);
+		address = getAddressDDRAM();
+		transmitUART('0'); 
+		transmitUART('b');
+		
+		for(int i = 7; i >= 0; i--){
+			if((address & (1 << i)) >> i){
+				transmitUART('1');
+			}
+			else{
+				transmitUART('0');
+			}
+		}
+		transmitUART('\r');
+		transmitUART('\n');
+		
+}
+
+static inline void morseProg(){
+    //debounceButton();
+    checkButton();
+}
+
+#endif
